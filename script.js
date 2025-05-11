b8ad73f911ab41de86a6a64c8a04b73f3648daa0
Futhia Nanda <futhiananda2016@gmail.com>
2025-05-12 01:46:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const name = "Futhia";
+    console.log("Selamat datang di website " + name + "!");
+    alert("Halo!Terima kasih sudah mengunjungi profil Futhia!");
+  });
